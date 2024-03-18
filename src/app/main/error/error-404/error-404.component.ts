8482d866eb9bb50c47f26035314342fd4dde9845
TuBui <bhtu1234@gmail.com>
2024-03-18 22:33:10 +0700

    update menu

--- src/app/main/error/error-404/error-404.component.ts
+++ src/app/main/error/error-404/error-404.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-error-404',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './error-404.component.html',
   styleUrl: './error-404.component.css'
 })
