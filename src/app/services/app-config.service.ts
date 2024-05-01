8d8bedb2c792f93f8ab0fd8a7a3118ea5c3912aa
TuBui <bhtu1234@gmail.com>
2024-05-01 16:49:29 +0700

    fix

--- src/app/services/app-config.service.ts
+++ src/app/services/app-config.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class AppConfigService {
 
   /* This BASE_URL will be red from assets/app-config/app.config.json */
-  BASE_URL: string = "https://localhost:40404";
+  BASE_URL: string = "https://localhost:44360";
   LANGUAGE!: boolean; // default language false => vn - true => en
   /********************************************************************/
 
